899a054b6be13a492651a94c2632519e0ef81167
SpacialGuyAlberto <lalbertomurillo1996@gmail.com>
2026-08-10 22:08:09 +0200

    Hazard Entity created

--- src/main/java/com/quarkbau/monolith/planning/model/Hazard.java
+++ src/main/java/com/quarkbau/monolith/planning/model/Hazard.java
@@ -1,7 +1,10 @@
 package com.quarkbau.monolith.planning.model;
 
+import com.quarkbau.monolith.auth.model.User;
 import jakarta.persistence.*;
 import lombok.*;
+
+import java.time.LocalDateTime;
 import java.util.UUID;
 
 
@@ -22,5 +25,29 @@ public class Hazard {
     @JoinColumn(name = "segment_id", nullable = false)
     private Segment segment;
 
+    @Column(name = "description")
+    private String description;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "reported_by")
+    private User reportedBy;
+
+    @Enumerated(EnumType.STRING)
+    private HazardStatus status;
+
+    @Enumerated(EnumType.STRING)
+    private HazardSeverityLevel severity;
+
+    @Enumerated(EnumType.STRING)
+    private HazardType type;
+
+    @Enumerated(EnumType.STRING)
+    private LocalDateTime createdAt;
+
+    @Enumerated(EnumType.STRING)
+    private LocalDateTime updatedAt;
+
+    @Column(name = "photo_evidence_url")
+    private String photoEvidenceUrl;
 
 }
